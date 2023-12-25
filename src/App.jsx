0e7a5e251ff83c13fd903ac416a295c9d357ff98
Rakesh Deka <58238022+rakeshdeka@users.noreply.github.com>
2023-12-25 14:48:18 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,50 @@
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import BarChart from './components/BarChart'
+import {UserData} from "./Data"
+import LineChart from './components/LineChart'
+import PieChart from './components/PieChart'
+import DoughnutChart from './components/DoughnutChart'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [userData, setUserData] = useState({
+    labels:UserData.map((data)=>data.year),
+    datasets:[{
+      label:"Users Gained",
+      data: UserData.map((data)=>data.userGain),
+      backgroundColor:[
+        "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+      ],
+      borderColor:["black"],
+      borderWidth:2
+    }]
+  })
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='App' style={{display:"flex", flexWrap: 'wrap'}}>
+      <div style={{width:"500px", height:"300px"}}>
+        <BarChart chartData={userData}/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div style={{width:"500px", height:"300px"}}>
+        <LineChart chartData={userData}/>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div style={{width:"500px" ,height:"300px"}}>
+        <PieChart chartData={userData}/>
+      </div>
+       <div style={{width:"500px" ,height:"300px"}}>
+        <DoughnutChart chartData={userData}/>
+      </div>
+
+      
+
+      
+    </div>
   )
 }
 
